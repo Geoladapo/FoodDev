08fdc8c140191a1ab4d98fac7dc427761f15c614
oladapo <oladapoelijah49@gmail.com>
2025-01-22 09:35:37 +0100

    db connection

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import * as express from 'express';
+import * as mongoose from 'mongoose';
+import { getEnvironmentVariable } from './environments/environment';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 let app: express.Application = express();
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
+});
+
+mongoose.connect(getEnvironmentVariable().db_uri).then(function () {
+  console.log('connected to mongodb');
 });
