e663b602dc502f600311fc951013957684a702cd
oladapo <oladapoelijah49@gmail.com>
2025-01-22 13:13:54 +0100

    added controller logic for login

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import { UserController } from '../controllers/user.controller';
+
+class UserRouter {
+  public router: Router;
+
+  constructor() {
+    this.router = Router();
+    this.getRoutes();
+    this.postRoutes();
+    this.patchRoutes();
+    this.putRoutes();
+    this.deleteRoutes();
+  }
+
+  getRoutes() {
+    this.router.get('/login', UserController.login);
+  }
+  postRoutes() {}
+  patchRoutes() {}
+  putRoutes() {}
+  deleteRoutes() {}
+}
+
+export default new UserRouter().router;
